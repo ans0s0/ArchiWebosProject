028c510e21fda4603a3b8b0c3a52581e429edc1f
A LENGLARE <a.lenglare@gmail.com>
2023-11-15 16:45:58 +0100

    Ajout fichier Javascript Login

---
+++ Backend/login.js
@@ -0,0 +1,29 @@
+//Je sélectionne le formulaire
+
+let form = document.querySelector("form");
+
+//J'ajoute un évènement au clic sur le bouton Envoyer
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); //J'annule le rechargement automatique du formulaire
+
+  let baliseEmail = document.getElementById("myEmail"); //Je pointe vers ID Email
+  let myEmail = baliseEmail.value; //Je récupère la valeur renseignée dans le champs Email
+
+  let balisePassword = document.getElementById("myPassword"); //Je pointe vers ID Password
+  let myPassword = balisePassword.value; //Je récupère la valeur renseignée dans le champs MDP
+
+  console.log(myEmail, myPassword); //J'affiche l'email et Password
+
+  //Penser à rajouter des règles pour la syntaxe de l'email
+});
+
+const response = await fetch("http://localhost:8081/users/login", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    email: "myEmail",
+    password: "myPassword",
+  }),
+})
+  .then(res.json())
+  .then((json) => console.log(response));
